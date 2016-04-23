bf4a64d0da229804651d86eb0af750c86aa1ba5a
Damenus <darczuk.damian@gmail.com>
2016-04-23 10:14:32 +0200

    Start

---
+++ CisLab1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CisLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine(args[0]);
+            Console.ReadKey();
+
+
+        }
+    }
+}
